e9e4fa40d7ca688c463c38617b305c9c7219bec4
Phil Broadley <philbrocoding@hotmail.com>
2023-03-08 16:01:21 +0000

    view bookings added

--- client/src/App.js
+++ client/src/App.js
@@ -4,34 +4,35 @@ import AddBooking from "./containers/AddBooking";
 import Contact from "./containers/Contact";
 import ViewBookings from "./containers/ViewBookings";
 import NavBar from "./components/NavBar";
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route
-} from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { getBookings } from "./BookingService";
 
 const App = () => {
-
-  const [bookings, setBookings] = useState([])
+  const [bookings, setBookings] = useState([]);
 
   useEffect(() => {
     getBookings().then((allBookings) => {
       setBookings(allBookings);
-    })
-  }, [])
+    });
+  }, []);
 
   const addBooking = (booking) => {
     setBookings([...bookings, booking]);
-  }
+  };
 
   return (
     <Router>
       <NavBar />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/add-booking" element={<AddBooking  addBooking={addBooking}/>} />
-        <Route path="/view-bookings" element={<ViewBookings />} />
+        <Route
+          path="/add-booking"
+          element={<AddBooking addBooking={addBooking} />}
+        />
+        <Route
+          path="/view-bookings"
+          element={<ViewBookings bookings={bookings} />}
+        />
         <Route path="/contact" element={<Contact />} />
       </Routes>
     </Router>
