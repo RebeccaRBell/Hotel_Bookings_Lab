f8e19e541a532437e7ea1bdef11e5de0593c53e8
Eliza Peterson <121064539+ElizaPeterson@users.noreply.github.com>
2023-03-08 15:23:45 +0000

    Booking Form Added

--- client/src/containers/AddBooking.js
+++ client/src/containers/AddBooking.js
@@ -1,8 +1,64 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 
-const AddBooking = () => {
+const AddBooking = ({postBooking, addBooking}) => {
+
+const [formData, setFormData] = useState({
+  name: "",
+  email: "",
+  date: ""
+});
+
+const onChange = (event) => {
+  const newFormData = Object.assign({}, formData);
+  newFormData[event.target.name] = event.target.value;
+  setFormData(newFormData);
+}
+
+const onSubmit = (event) => {
+  event.preventDefault();
+  postBooking(formData).then((data) => {
+    addBooking(data);
+  })
+  setFormData({
+    name: "",
+    email: "",
+    date: ""
+  });
+}
+
   return (
-    <div>AddBooking</div>
+    <form onSubmit={onSubmit} id="bookings-form" >
+    <h2>Add a Booking</h2>
+    <div className="formWrap">
+        <label htmlFor="name">Name:</label>
+        <input 
+            onChange={onChange} 
+            type="text" 
+            id="name" 
+            name="name"
+            value={formData.name} />
+    </div>
+    <div className="formWrap">
+        <label htmlFor="email">Email:</label>
+        <input 
+            onChange={onChange} 
+            type="text" 
+            id="email" 
+            name="email"
+            value={formData.email} />
+    </div>
+    <div className="formWrap">
+        <label htmlFor="date">Date:</label>
+        <input 
+            onChange={onChange} 
+            type="date" 
+            id="date" 
+            name="date" 
+            value={formData.date}/>
+    </div>
+
+    <input type="submit" value="Save" id="save"/>
+</form>
   )
 }
 
